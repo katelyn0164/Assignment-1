1064a05b8ff214cc419a207557ab7c0495c8e0eb
Katelyn Dempsey <katelyn123@live.com>
2024-01-13 08:50:48 -0800

    Initial Commit

---
+++ Assignment 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 1
+//
+//  Created by user249277 on 1/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
